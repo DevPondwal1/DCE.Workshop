093d487849c462a83f3a4ff50602d59e198caf50
DevPondwal1 <159519705+DevPondwal1@users.noreply.github.com>
2024-02-09 14:19:45 +0530

    calculator.sol

---
+++ calculator.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+contract cal{
+    
+    int i =10;
+    uint j =120;
+    function display (int i) public view returns(int){
+        return i;
+    }
+    function addition (int x ,int y) public view returns (int){
+        return x+y;
+    
+    }
+    function addition (int x ,int y) public view returns (int){
+        return x-y;
+    
+    }
+    function addition (int x ,int y) public view returns (int){
+        return x*y;
+    
+    }
+    function addition (int x ,int y) public view returns (int){
+        return x/y;
+    
+    }
+
+
+
+
+} 
